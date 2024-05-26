e309ca1cfb75c16ff56ba630279ac77c82064039
mayaspiek <maya.spieker@gmail.com>
2024-05-26 20:02:04 -0300

    ajeitando rotas e estilizacao

---
+++ src/pages/mvp2/MVP2.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MVP2 = () => {
+    return (
+        <div>
+            <h2>
+                Implementação em MVP2
+            </h2>
+        </div>
+    )
+}
