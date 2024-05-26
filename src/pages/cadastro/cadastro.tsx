d49fdb31fae699500ef6ef5d94b35774ee6ac3ca
Eliana100 <nunessilva347@gmail.com>
2024-05-26 15:49:33 -0300

    Estilização do cadastro 1

--- src/pages/cadastro/cadastro.tsx
+++ src/pages/cadastro/cadastro.tsx
@@ -3,12 +3,11 @@ import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import "./cadastro.styles.css";
 import { Gender, Role, User } from "../../models/User";
-import { UserLoginService } from "../../api/services/UserLoginService";
-import { UserLoginRepository } from "../../api/repositories/UserLoginRepository";
 import { AxiosHttpClient } from "../../api/AxiosHttpClient";
 import { UserRegisterService } from "../../api/services/UserRegisterService";
 import { UserRegisterRepository } from "../../api/repositories/UserRegisterRepository";
-import { Button } from "../../components/button/Button";
+import backgroundImage from '../../assets/img/cadastro_tela.png';
+import nomeLogo from '../../assets/img/logo-compl.png';
 
 
 function Cadastro() {
@@ -16,8 +15,8 @@ function Cadastro() {
     const [email, setEmail] = useState("");
     const [nome, setNome] = useState("");
     const [nick, setNick] = useState("");
-    const [gender, setGender] = useState<Gender>();
-    const [role, setRole] = useState<Role>();
+    const [gender, setGender] = useState<Gender>(Gender.FEMININO);
+    const [role, setRole] = useState<Role>(Role.ROLE_USER);
     const [confirmPassword, setConfirmPassword] = useState("");
 
     const [user, setUser] = useState<User>({
@@ -69,100 +68,101 @@ function Cadastro() {
 
     return (
         <div className="container">
-            <form className="form" onSubmit={handleSubmit}>
-                <h2>Cadastro</h2>
-                <input
-                    type="text"
-                    placeholder="Nome"
-                    required
-                    className="input"
-                    value={nick}
-                    onChange={handleChanges}
-                />
-                <input
-                    type="text"
-                    placeholder="Nick"
-                    required
-                    className="input"
-                    value={nome}
-                    onChange={handleChanges}
-                />
-                <div className="checkBox-conainer">
-                    <div>
-                        <label htmlFor="feminino">Femino</label>
-                        <input
-                            checked={gender === "feminino"}
-                            onChange={() => handleSelectGender(Gender.FEMININO)}
-                            type="checkbox"
-                            name="feminino"
-                            id="feminino"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="masculino">Masculino</label>
-                        <input
-                            checked={gender === "masculino"}
-                            onChange={() => handleSelectGender(Gender.MASCULINO)}
-                            type="checkbox"
-                            name="masculino"
-                            id="masculino"
-                        />
+            <img src={backgroundImage} className="background"/>
+              <form className="form" onSubmit={handleSubmit}>
+                <img src={nomeLogo} alt="Logo" className="logo" />
+                <h2 className="Titulo">Realize o seu cadastro</h2>
+                <p>Preencha os campos abaixo com os seus dados para criar sua conta</p>
+                <div className="input-container">
+                    <label htmlFor="role">Como você deseja acessar a plataforma?</label>
+                    <div className="role-options">
+            
+
+                            <button className={ role === Role.ROLE_USER ? "btn-check" : "btn-button"}onClick={()=>handleRole(Role.ROLE_USER)}>Gamer</button>
+            
+                       
+                            
+                            <button className={ role === Role.ROLE_ORGANIZADOR ? "btn-check" : "btn-button"} onClick={()=>handleRole(Role.ROLE_ORGANIZADOR)}>Organizador</button>
+                       
                     </div>
                 </div>
-
-                <div className="checkBox-conainer">
-                    <div>
-                        <label htmlFor="feminino">Organizador</label>
-                        <input
-                            checked={role === "ROLE_ORGANIZADOR"}
-                            onChange={() => handleRole(Role.ROLE_ORGANIZADOR)}
-                            type="checkbox"
-                            name="organizador"
-                            id="organizador"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="masculino">Usuario</label>
-                        <input
-                            checked={role === "ROLE_USER"}
-                            onChange={() => handleRole(Role.ROLE_USER)}
-                            type="checkbox"
-                            name="usuario"
-                            id="usuario"
-                        />
+                <div className="input-container">
+                    <label htmlFor="genero">Qual o seu gênero?</label>
+                    <div className="gender-options">
+              
+                            
+                    <button className={ gender === Gender.FEMININO ? "btn-check" : "btn-button"}onClick={()=>handleSelectGender(Gender.FEMININO)}>Feminino</button>
+                
+                            
+                    <button className={ gender === Gender.MASCULINO ? "btn-check" : "btn-button"}onClick={()=>handleSelectGender(Gender.MASCULINO)}>Masculino</button>
                     </div>
                 </div>
-
-                <input
-                    type="email"
-                    placeholder="Email"
-                    required
-                    className="input"
-                    value={email}
-                    onChange={handleChanges}
-                />
-                <input
-                    type="password"
-                    placeholder="Senha"
-                    required
-                    className="input"
-                    value={password}
-                    onChange={handleChanges}
-                />
-                <input
-                    type="password"
-                    placeholder="Confirme a Senha"
-                    required
-                    className="input"
-                    value={confirmPassword}
-                    onChange={handleChanges}
-                />
-                <Button href={'/registro'}>
-                    Cadastrar
-                </Button>
+                <div className="input-container">
+                    <label htmlFor="nome">Nome</label>
+                    <input
+                        type="text"
+                        placeholder="Nome"
+                        required
+                        className="input"
+                        value={nome}
+                        onChange={handleChanges}
+                        name="name"
+                    />
+                </div>
+                <div className="input-container">
+                    <label htmlFor="nick">Username</label>
+                    <input
+                        type="text"
+                        placeholder="Nick"
+                        required
+                        className="input"
+                        value={nick}
+                        onChange={handleChanges}
+                        name="username"
+                    />
+                </div>
+                <div className="input-container">
+                    <label htmlFor="email">E-mail</label>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        required
+                        className="input"
+                        value={email}
+                        onChange={handleChanges}
+                        name="email"
+                    />
+                </div>
+                <div className="input-container">
+                    <label htmlFor="senha">Senha</label>
+                    <input
+                        type="password"
+                        placeholder="Senha"
+                        required
+                        className="input"
+                        value={password}
+                        onChange={handleChanges}
+                        name="password"
+                    />
+                </div>
+                <div className="input-container">
+                    <label htmlFor="confirmarSenha">Confirme a Senha</label>
+                    <input
+                        type="password"
+                        placeholder="Confirme a Senha"
+                        required
+                        className="input"
+                        value={confirmPassword}
+                        onChange={handleChanges}
+                        name="confirmPassword"
+                    />
+                </div>
+                <button type="submit" className="submit">
+  Cadastrar
+</button>
                 
                 <Link to="/login">
-                    <button className="btn-voltar">Voltar</button>
+                    <button className="voltar">Voltar</button>
                 </Link>
             </form>
         </div>
